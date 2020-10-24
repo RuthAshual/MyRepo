869addf5d3e3d9f24189f70bec5187aba3a6d415
Rut-Ashual <ruti3822@gmail.com>
2020-10-25 00:24:19 +0300

    change summeries list

--- TestProj/Controllers/SampleDataController.cs
+++ TestProj/Controllers/SampleDataController.cs
@@ -11,7 +11,7 @@ namespace TestProj.Controllers
     {
         private static string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Scorching2"
         };
 
         [HttpGet("[action]")]
